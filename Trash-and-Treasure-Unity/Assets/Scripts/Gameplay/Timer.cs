e88261fba22a0bb8e33be68a9a834066917e1fb3
dafydddev <85412836+dafydddev@users.noreply.github.com>
2025-08-08 11:32:49 +0100

    Introduced new timer system

---
+++ Trash-and-Treasure-Unity/Assets/Scripts/Gameplay/Timer.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace Gameplay
+{
+    [RequireComponent(typeof(TMPro.TMP_Text))]
+    public class Timer : MonoBehaviour
+    {
+        [Header("Times")]
+        [SerializeField] 
+        private float endTime;
+        [SerializeField] 
+        private float startTime = 100;
+        
+        private float _time;
+        
+        private TMPro.TMP_Text _timerText;
+
+        private void Awake()
+        {
+            _time = startTime;
+            _timerText = GetComponent<TMPro.TMP_Text>();
+            _timerText.text = _time.ToString("0");
+
+        }
+
+        private void Update()
+        {
+            if (!(_time > endTime) || GameEvents.IsPaused) return;
+            _time -= Time.deltaTime;
+            _timerText.text = _time.ToString("0");
+        }
+
+    }
+}

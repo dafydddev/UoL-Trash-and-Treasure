8e3557d7c14ca3a9757d2914b54aedafe72842f4
dafydddev <85412836+dafydddev@users.noreply.github.com>
2025-08-16 21:34:59 +0100

    Screen Fading Animation (bug fixs)

--- Trash-and-Treasure-Unity/Assets/Scripts/UI/FadeInImage.cs
+++ Trash-and-Treasure-Unity/Assets/Scripts/UI/FadeInImage.cs
@@ -1,28 +1,29 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
-[RequireComponent(typeof(Animator))]
-public class FadeInImage : MonoBehaviour
-{ 
-    private Animator _animator;
-    [SerializeField] private AnimationClip fadeInAndOutClip;
+namespace UI
+{
+    [RequireComponent(typeof(Animator))]
+    public class FadeInImage : MonoBehaviour
+    { 
+        private Animator _animator;
+        [SerializeField] private AnimationClip fadeInAndOutClip;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        SceneManager.sceneLoaded += OnSceneLoaded;
-        _animator = GetComponent<Animator>();
-    }
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+            _animator = GetComponent<Animator>();
+        }
 
-    private void OnDestroy()
-    {
-        SceneManager.sceneLoaded -= OnSceneLoaded;
-    }
+        private void OnDestroy()
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
 
-    private void OnSceneLoaded(Scene _, LoadSceneMode __)
-    {
-        Debug.Log("Scene loaded");
-        _animator.Play(fadeInAndOutClip.name, 0, 0f);
+        private void OnSceneLoaded(Scene _, LoadSceneMode __)
+        {
+            _animator.Play(fadeInAndOutClip.name, 0, 0f);
+        }
     }
 }

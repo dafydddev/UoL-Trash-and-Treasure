d2850d89363abcdf6b58c40c83f50f83df685fb0
dafydddev <85412836+dafydddev@users.noreply.github.com>
2025-08-23 22:53:18 +0100

    Game State (Level Complete and Level Failed) UI logic

--- Trash-and-Treasure-Unity/Assets/Scripts/UI/GameEndUI.cs
+++ Trash-and-Treasure-Unity/Assets/Scripts/UI/GameEndUI.cs
@@ -6,14 +6,17 @@ using UnityEngine.Serialization;
 namespace UI
 {
     [RequireComponent(typeof(Animator))]
-    public class GameStateUI : MonoBehaviour
+    public class GameEndUI : MonoBehaviour
     {
         [SerializeField] private GameObject retryButton; 
         [SerializeField] private GameObject nextButton; 
         [SerializeField] private GameObject mainMenuButton;
         
         [SerializeField] private string gameStateTrigger = "GameOver";
-        [SerializeField] private TMP_Text gameStateText;
+        [SerializeField] private TMP_Text gameStateText; 
+        [SerializeField] private TMP_Text quoteText;
+        [SerializeField] private string[] levelWinQuotes; 
+        [SerializeField] private string[] levelLoseQuotes;
         private Animator _animator;
         
         private void Start()
@@ -33,7 +36,11 @@ namespace UI
         private void HandleGameOver()
         {
             GameEvents.SetGameInProgress(false);
-            gameStateText.text = "Game Over";
+            if (gameStateText)
+            {
+                gameStateText.text = "Game Over";
+            }
+            quoteText.text = GetRandomQuote(levelLoseQuotes);
             _animator.SetTrigger(gameStateTrigger);
             nextButton.SetActive(false);
             mainMenuButton.SetActive(true);
@@ -43,11 +50,20 @@ namespace UI
         private void HandleLevelComplete()
         {
             GameEvents.SetGameInProgress(false);
-            gameStateText.text = "Level Complete";
+            if (gameStateText)
+            {
+                gameStateText.text = "Level Complete";
+            }
+            quoteText.text = GetRandomQuote(levelWinQuotes);
             _animator.SetTrigger(gameStateTrigger);
             nextButton.SetActive(true);
             mainMenuButton.SetActive(true);
             retryButton.SetActive(false);       
+        }
+
+        private string GetRandomQuote(string[] quotes)
+        {
+            return quotes.Length == 0 ? "" : quotes[Random.Range(0, quotes.Length)];
         }
     }
 }

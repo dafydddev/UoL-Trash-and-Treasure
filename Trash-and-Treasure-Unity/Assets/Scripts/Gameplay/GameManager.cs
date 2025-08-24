ddf09fd97c8b3334f5ff3995809efc49158f6ba5
dafydddev <85412836+dafydddev@users.noreply.github.com>
2025-08-24 21:05:51 +0100

    Fixed merge fix - removing Audio Manager logic from game manager

--- Trash-and-Treasure-Unity/Assets/Scripts/Gameplay/GameManager.cs
+++ Trash-and-Treasure-Unity/Assets/Scripts/Gameplay/GameManager.cs
@@ -86,17 +86,7 @@ namespace Gameplay
             {
                 GameEvents.SetHasCompletedTutorial(true);
             }
-            else if (scene.name != "MainMenu")
-            {
-                // If we are not in the main menu, stop the scene audio
-                // We will later trigger it to start when the player starts the gameplay
-                AudioManager.Instance.StopSceneAudio();
-            }
-            else
-            {
-                // If we are in the main menu, play the main menu background music
-                AudioManager.Instance.PlayMainMenuBackground();
-            }
+
         }
 
         private static void HandleGameStart()
